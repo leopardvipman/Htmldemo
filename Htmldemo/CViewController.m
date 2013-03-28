e1158999860a9f8e9b70bcbeadb9f89ca0d30405
hongqiangyang <leopardvipman@gmail.com>
2013-03-28 16:41:21 +0800

    htmldemo
    htmldemo

--- Htmldemo/CViewController.m
+++ Htmldemo/CViewController.m
@@ -17,7 +17,24 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    // Do any additional setup after loading the view, typically from a nib.
+    UIWebView *webView = [[UIWebView alloc] initWithFrame:self.view.frame];
+    
+    //使用loadHTMLString()方法显示网页内容
+//    [webView loadHTMLString:[self getHtmlString] baseURL:nil];
+    NSString*strng=@"<head><meta http-equiv=\"Content-Type\" content=\"text/html; charset=gb2312\" /><title>无标题文档</title></head><body>dhsdhdfh</body></html>";
+    [webView loadHTMLString:strng baseURL:nil];
+//    [webView loadHTMLString:[self getHtmlString] baseURL:[NSURL fileURLWithPath:[[NSBundle mainBundle] resourcePath]]];
+//    
+    [self.view addSubview:webView];
+}
+//读取html文件内容
+- (NSString *)getHtmlString{
+    //文件路径
+    NSString *filePath = [[NSBundle mainBundle] pathForResource:@"index" ofType:@"html"];
+    
+    NSString *contents = [NSString stringWithContentsOfFile:filePath encoding:NSUTF8StringEncoding error:nil];
+    return contents;
 }
 
 - (void)didReceiveMemoryWarning
